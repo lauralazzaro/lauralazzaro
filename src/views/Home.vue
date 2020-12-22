d98106f666a94aca629502a743615ca85b61ea3e
Laura <lz_laura@hotmail.com>
2020-12-22 23:21:18 +0100

    Under construction

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <UnderConstruction msg="Website under construction" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import UnderConstruction from '@/components/UnderConstruction.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    UnderConstruction
+  }
+};
+</script>
