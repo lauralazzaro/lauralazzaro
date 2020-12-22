909f891e92cc7ab679ff5a65e7c71d963a32edce
Laura <lz_laura@hotmail.com>
2020-12-22 23:49:13 +0100

    Import from under construction to start dev

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="overlay"></div>
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UnderConstruction',
+  props: ['msg']
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 6vw;
+}
+</style>
